b5642fd5e94f44f2e21d296656ea8c555b476f75
Jeff Lamb <jefflamb@gmail.com>
2020-07-09 01:00:35 -0700

    Simplified the button onclick code

--- popup/popup.js
+++ popup/popup.js
@@ -1,28 +1,21 @@
-const sendMessageId = document.getElementById("sendmessageid");
-if (sendMessageId) {
-    sendMessageId.onclick = function() {
-        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-            // set a timer to close the tab after the specified time
-            setTimeout(function() { 
-                chrome.tabs.remove(tabs[0].id);
-            }, getCloseTimeInSeconds()*1000);
+$("#setTimer").bind('click', function(e){
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        // set a timer to close the tab after the specified time
+        setTimeout(function() { 
+            chrome.tabs.remove(tabs[0].id);
+        }, getCloseTimeInSeconds()*1000);
 
-            function getCloseTimeInSeconds() {
-                var seconds = 0;
-                
-                seconds += Number($("#seconds")[0].value);
-                seconds += Number($("#minutes")[0].value * 60);
-                seconds += Number($("#hours")[0].value * 60 * 60);
+        function getCloseTimeInSeconds() {
+            var seconds = 0;
             
-                return seconds;
-            }
-        });
-    };
-}
-
-//$("#sendMessageId").bind('click', function(e){
-
-//});
+            seconds += Number($("#seconds")[0].value);
+            seconds += Number($("#minutes")[0].value * 60);
+            seconds += Number($("#hours")[0].value * 60 * 60);
+        
+            return seconds;
+        }
+    });
+});
 
 $(":text").bind('mousewheel', function(e){
     // Increase text inputs by 1 on mousewheelup, down by 1 on mousewheel down
