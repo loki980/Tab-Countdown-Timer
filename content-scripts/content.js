20e0a530245335b578d135be51c94c2010333d51
Jeff Lamb <jefflamb@gmail.com>
2020-07-09 00:33:34 -0700

    Icon updated, old demo code and directories removed.

--- content-scripts/content.js
+++
@@ -1,7 +0,0 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    setTimeout(function() { 
-        myWindow.close()
-    }, request.autoCloseTimeInSeconds);
-
-    sendResponse({ fromcontent: "This message is from content.js" });
-});
