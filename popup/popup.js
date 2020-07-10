647a53f1e87334ad511cfa3fa7ffcb5a77971a71
Jeff Lamb <jefflamb@gmail.com>
2020-07-09 22:58:40 -0700

    Add time remaining to the popup

--- popup/popup.js
+++ popup/popup.js
@@ -54,3 +54,36 @@ function fixOverflowAndUnderflows() {
         $("#hours")[0].value++;
     }
 }
+
+chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.alarms.get(tabs[0].id.toString(), function(alarms) {
+        if(alarms != null) {
+            var countDownDate = new Date(alarms.scheduledTime).getTime();
+            
+            // Update the count down every 1 second
+            var x = setInterval(function() {
+                // Get today's date and time
+                var now = new Date().getTime();
+                
+                // Find the distance between now and the count down date
+                var distance = countDownDate - now;
+                
+                // Time calculations for days, hours, minutes and seconds
+                var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+                var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+                
+                // Output the result in an element with id="timeRemaining"
+                document.getElementById("timeRemaining").innerHTML = days + "d " + hours + "h "
+                + minutes + "m " + seconds + "s ";
+                
+                // If the count down is over, write some text 
+                if (distance < 0) {
+                    clearInterval(x);
+                    document.getElementById("timeRemaining").innerHTML = "EXPIRED";
+                }
+            }, 1000);
+        }
+    });
+});
